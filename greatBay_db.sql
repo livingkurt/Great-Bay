8b2bf68ac233a274fd87e1a492511ede22737012
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-27 14:22:45 -0600

    added some items

--- greatBay_db.sql
+++ greatBay_db.sql
@@ -6,31 +6,39 @@ USE greatBay_db;
 
 CREATE TABLE items
 (
-  id INT NOT NULL AUTO_INCREMENT, 
-  itemName VARCHAR(45) NOT NULL,
+  id INT NOT NULL
+  AUTO_INCREMENT, 
+  itemName VARCHAR
+  (45) NOT NULL,
   startingBid INT NULL,
   currentBid INT NULL,
 );
 
   INSERT INTO items
-    (itemName, startingBid, currentBid)
+    (itemName, startingBid)
   VALUES
     ("2007 Honda Civic", "2500");
 
   INSERT INTO items
     (itemName, startingBid, currentBid)
   VALUES
-    ("2013 Hyandai Santa Fe", "7000");
+    ("2013 Hyandai Santa Fe", 7000, 7100);
+
+  INSERT INTO items
+    (itemName, startingBid)
+  VALUES
+    ("2006 Dodge Ram", 5000);
+
+  INSERT INTO items
+    (itemName, startingBid)
+  VALUES
+    ("2020 Toyota Land Cruiser", 85000);
 
   INSERT INTO items
     (itemName, startingBid, currentBid)
   VALUES
-    ("2006 Dodge Ram", "5000");
-
-  INSERT INTO items
-    (itemName, startingBid, currentBid)
-  VALUES
-    ("2020 Toyota Land Cruiser", "85000");
+    ("Weeeeeeeeeed, 1lb", 20, 25);
 
 
-  SELECT * FROM items;
+  SELECT *
+  FROM items;
