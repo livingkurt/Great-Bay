1043623d0922571a1186783869260edefa5c5b6b
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-27 13:50:10 -0600

    added first question

--- index.js
+++ index.js
@@ -6,7 +6,17 @@ require("dotenv").config();
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 
+
+// When the user runs node index.js, this prompts the user.
 inquirer
     .prompt([
-
+        // Main Menu
+        type: "list",
+        name: "mainMenu",
+        message: "Welcome to the Great Bay. Please choose one of the options below."
+        choices: [
+            "List an item",
+            "Bid on an item",
+            "Exit"
+        ]
     ])
