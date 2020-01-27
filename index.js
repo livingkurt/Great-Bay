4e3520cf3dba66482d28d293bdda62af3e4eb204
Crystal Ly <crystal.m.ly@gmail.com>
2020-01-27 13:45:33 -0600

    merge

---
+++ index.js
@@ -0,0 +1,12 @@
+/*===================================================================
+    Welcome to the Great Bay! For all of your pharmaceutical needs!
+================================================================== */
+
+require("dotenv").config();
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+
+inquirer
+    .prompt([
+
+    ])
